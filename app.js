50dd18d7d096a69ae2a4f284e66c7d62673f1d09
leekichang <1kichang@yonsei.ac.kr>
2021-01-06 01:16:45 +0900

    added profile image

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const title = document.getElementById("myTitle");
-
+const image = document.getElementById("myImage");
 
 function handleClick(event){
     location.href = "C:/Users/Administrator/Desktop/Java_Script/Profile-Page/pages/life.html";
