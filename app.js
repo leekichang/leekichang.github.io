1199fac00ad02533f0084603271f2141fb62eb6e
leekichang <1kichang@yonsei.ac.kr>
2021-01-06 00:22:15 +0900

    add link function

---
+++ app.js
@@ -0,0 +1,8 @@
+const title = document.getElementById("myTitle");
+
+
+function handleClick(event){
+    location.href = "C:/Users/Administrator/Desktop/Java_Script/Profile-Page/pages/life.html";
+}
+
+title.addEventListener("click", handleClick);
